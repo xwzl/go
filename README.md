5f62292bd2d179c6fb9a8b3e3a5043a7f97d5cc6
徐伟智 <l>
2019-08-16 16:47:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 基本命令
+    
+    // 运行
+    go run demo.go
+    
+    // 编译二进制文件
+    go build demo.go
+    
+    // 
---
+++ demo.go
@@ -0,0 +1,23 @@
+package main
+
+// 常量定义
+const PI = 3.14
+
+// 全局变量的声明和赋值
+var name = "gopher"
+
+// 一般类型声明
+type newType int
+
+// 结构的声明
+type gopher struct{}
+
+// 接口的声明
+type golang interface{}
+
+// 由main函数作为程序入口点启动
+func main() {
+
+	println("Hello World!" + name)
+
+}
